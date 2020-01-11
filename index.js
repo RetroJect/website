56d2d5cb5e000b211548775650b2c2206760d607
Grayson Doshier <20195966+RetroJect@users.noreply.github.com>
2020-01-10 16:58:29 -0800

    Added Projects Setion
    + Changed package version to 2.0.0
    + github-api from npm
    + Added github repos card with github-api
    + Updated home page
    + Fomantic Auto Install

--- index.js
+++ index.js
@@ -7,7 +7,58 @@
 `----' File: index.js
 */
 
+/*
+-= Requires =-
+*/
+
+const GitHub = require('github-api');
 const express = require('express');
+
+
+/*
+-= GitHub Setup =-
+*/
+var gh = new GitHub();
+var me = gh.getUser('RetroJect');
+var repoData = new Array();
+
+// Get a list of my repositories sorted by date last pushed
+function getRepos() {
+  me.listRepos({ sort: 'pushed' }, (err, repos) => {
+    if (err) {
+      console.log(err)
+    } else {
+      for (var i = 0; i < repos.length; i++) {
+        var pushedDate = new Date(repos[i]['pushed_at']);
+        var createdDate = new Date(repos[i]['created_at']);
+        var dateOpts = {
+          weekday: 'long',
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+          hour: 'numeric',
+          minute: 'numeric',
+          second: 'numeric'
+        }
+        repoData.push({
+          url: repos[i]['svn_url'],
+          pushed: pushedDate.toLocaleString("en-US", dateOpts),
+          created: createdDate.toLocaleString('en-US', dateOpts),
+          name: repos[i]['full_name'],
+          description: repos[i]['description']
+        })
+      }
+    }
+  });
+  // Take repos[#].svn_url and repos[#].pushed_at
+}
+
+getRepos();
+
+
+/*
+-= Express Setup =-
+*/
 const app = express();
 app.set('view engine', 'pug');
 const port = 3000;
@@ -25,7 +76,8 @@ const server = app.listen(port, () => {
 
 app.get('/', (req, res) => {
   res.render('home', {
-    title: 'Home'
+    title: 'Home',
+    data: repoData
   });
 });
 

497c8b112034c7965e7e55916f73a4b84abe3cce
Grayson Doshier <20195966+RetroJect@users.noreply.github.com>
2020-01-15 12:51:33 -0800

    Version 2
    + Underlined Link Animation
    + Smooth Scroll
    + Updated GitHub Cards
    + Updated jQuery version
    + Added text to other sections
    + Added 'About' Section

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,6 +1,17 @@
-$('[href*="#"').on('click', (e) => {
-  e.preventDefault();
-  $('html,body').animate({
-    scrollTop: $($(this).attr('href')).offset().top,
-  }, 500, 'linear')
+$('[href*="#"]').on('click', (e) => {
+  var target = $(this.hash);
+  if (target.length) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: target.offset().top
+    }, 0, 'swing')
+  }
 });
+
+$('#toTop').click(() => {
+  $('html, body').animate({
+    scrollTop: 0
+  }, 0, 'swing')
+})
+
+$('.popup').popup();
--- index.js
+++ index.js
@@ -13,6 +13,7 @@
 
 const GitHub = require('github-api');
 const express = require('express');
+const datediff = require('date-diff');
 
 
 /*
@@ -21,6 +22,16 @@ const express = require('express');
 var gh = new GitHub();
 var me = gh.getUser('RetroJect');
 var repoData = new Array();
+var dateOpts = {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+  second: 'numeric',
+  timeZoneName: 'long'
+}
 
 // Get a list of my repositories sorted by date last pushed
 function getRepos() {
@@ -28,32 +39,48 @@ function getRepos() {
     if (err) {
       console.log(err)
     } else {
+      delete repoData;
+      repoData = new Array();
       for (var i = 0; i < repos.length; i++) {
         var pushedDate = new Date(repos[i]['pushed_at']);
         var createdDate = new Date(repos[i]['created_at']);
-        var dateOpts = {
-          weekday: 'long',
-          year: 'numeric',
-          month: 'long',
-          day: 'numeric',
-          hour: 'numeric',
-          minute: 'numeric',
-          second: 'numeric'
+
+        // Get difference between last push and now
+        var now = new Date();
+        now.getDate();
+        var ddiff = new datediff(now, pushedDate);
+
+        var diff = "";
+        if(ddiff.seconds() < 60){
+          diff = 'seconds';
+        } else if(ddiff.seconds() < 3600){
+          diff = 'minutes';
+        } else if(ddiff.seconds() < 86400){
+          diff = 'hours';
+        } else if(ddiff.seconds() < 2678400){
+          diff = 'days';
+        } else if(ddiff.seconds() < 32140800){
+          diff = 'months';
+        } else {
+          diff = 'years';
         }
+
         repoData.push({
           url: repos[i]['svn_url'],
           pushed: pushedDate.toLocaleString("en-US", dateOpts),
           created: createdDate.toLocaleString('en-US', dateOpts),
           name: repos[i]['full_name'],
-          description: repos[i]['description']
+          description: repos[i]['description'],
+          avatar: repos[i]['owner']['avatar_url'],
+          difference: diff
         })
       }
     }
   });
-  // Take repos[#].svn_url and repos[#].pushed_at
 }
 
 getRepos();
+setInterval(getRepos, 600000)
 
 
 /*
